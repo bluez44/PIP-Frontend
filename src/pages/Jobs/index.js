65ebeea497bbd29cd3347df7c64c3b9b015c953d
unknown <vlqvinh444@gmail.com>
2024-11-20 10:38:19 +0700

    Finish Jobs page

--- src/pages/Jobs/index.js
+++ src/pages/Jobs/index.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import './style.css'
+import JobCard from '../../components/JobCard'
+import p1 from '../../img/product-1-720x480.jpg'
+
 
 export default function Jobs() {
   return (
-    <div>Jobs</div>
+    <div className='job__wrapper'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-3'>
+              Category
+            </div>
+            <div className='col-9 job__container--list'>
+              <div className='row'>
+                <div className='col-6'>
+                  <JobCard imgUrl={p1} location='Hanoi' title='Software Engineer' salary='10.000.000' role='Full-time'/>
+                </div>
+                <div className='col-6'>
+                  <JobCard imgUrl={p1} location='Hanoi' title='Software Engineer' salary='10.000.000' role='Full-time'/>
+                </div>
+                <div className='col-6'>
+                  <JobCard imgUrl={p1} location='Hanoi' title='Software Engineer' salary='10.000.000' role='Full-time'/>
+                </div>
+                <div className='col-6'>
+                  <JobCard imgUrl={p1} location='Hanoi' title='Software Engineer' salary='10.000.000' role='Full-time'/>
+                </div>
+                <div className='col-6'>
+                  <JobCard imgUrl={p1} location='Hanoi' title='Software Engineer' salary='10.000.000' role='Full-time'/>
+                </div>
+                <div className='col-6'>
+                  <JobCard imgUrl={p1} location='Hanoi' title='Software Engineer' salary='10.000.000' role='Full-time'/>
+                </div>
+              </div>              
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
